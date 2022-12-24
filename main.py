fc305ac1f94d4a9f36456ad76cab2afb740b1b55
Revo <ganandocriptomonedass@gmail.com>
2022-12-24 16:43:11 -0500

    request_reponse_body

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPO
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+
+#  Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get("/")  # path operation decorator
 def home():  # path operation function
     return {"hello": "World"}  # JSON
+
+
+#  Request and Response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
