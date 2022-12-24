df687ee380d71fcfe4bec4fafdf07eeae5c88e79
Revo <ganandocriptomonedass@gmail.com>
2022-12-24 18:17:42 -0500

    validations_body

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ app = FastAPI()
 
 #  Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -58,3 +63,20 @@ def show_person(
         person_id: int = Path(gt=0)
 ):
     return {person_id: "it exists!"}
+
+
+#  Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+        person_id: int = Path(
+            title="Person ID",
+            description="This is the person ID",
+            gt=0
+        ),
+        person: Person = Body(),
+        location: Location = Body()
+):
+    result = person.dict()
+    result.update(location.dict())
+    return result
